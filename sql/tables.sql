f12520cbe240fd1eaa7b2557fa9d9841a7650724
Joel Ferreira <joel_ferrer_94@hotmail.com>
2024-10-05 01:01:20 -0400

    fix: tables

--- sql/tables.sql
+++ sql/tables.sql
@@ -10,3 +10,27 @@ INSERT INTO products (name, description, price, image_url) VALUES
 ('Laptop', 'Gaming laptop ', 6000000, '/images/laptop.jpg'),
 ('Teclado', 'Teclado Gamer', 120000, '/images/keyboard.jpg'),
 ('Headset', 'Gaming Headset', 300000, '/images/headphones.jpg');
+
+
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  debt_id VARCHAR(255) NOT NULL,
+  total_amount INTEGER NOT NULL,
+  status VARCHAR(50) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE order_items (
+  id SERIAL PRIMARY KEY,
+  order_id INTEGER REFERENCES orders(id),
+  product_id INTEGER REFERENCES products(id),
+  quantity INTEGER NOT NULL
+);
+
+ALTER TABLE orders
+ADD COLUMN total_amount INTEGER;
+
+ALTER TABLE orders DROP COLUMN IF EXISTS amount;
+ALTER TABLE orders DROP COLUMN IF EXISTS total_amount;
+ALTER TABLE orders ADD COLUMN amount INTEGER NOT NULL;
