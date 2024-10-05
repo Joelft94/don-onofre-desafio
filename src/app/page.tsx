6c0539b87e208aaa483380cf2155aa2e54fa9ca9
Joel Ferreira <joel_ferrer_94@hotmail.com>
2024-10-05 01:01:55 -0400

    update: page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import ProductList from '@/components/ProductList';
+import Cart from '@/components/Cart';
 
 export default function Home() {
   return (
     <main className="container mx-auto px-4">
-      <h1 className="text-3xl font-bold my-8">Don Onofre's Products</h1>
-      <ProductList />
+      <h1 className="text-3xl font-bold my-8">Our Products</h1>
+      <div className="flex flex-col md:flex-row">
+        <div className="md:w-3/4 pr-4">
+          <ProductList />
+        </div>
+        <div className="md:w-1/4">
+          <Cart />
+        </div>
+      </div>
     </main>
   );
 }
