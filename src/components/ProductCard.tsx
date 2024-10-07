755d1349041d09c539b33614ca5799e0f06ab41b
Joel Ferreira <joel_ferrer_94@hotmail.com>
2024-10-06 22:57:15 -0300

    update: page and component styles

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,30 +1,52 @@
 'use client';
 
-import { useCart } from '@/contexts/CartContext';
+import React from 'react';
 import { Product } from '@/types';
-import Image from 'next/image'
+import { useCart } from '@/contexts/CartContext';
 
+interface ProductCardProps {
+  product: Product;
+}
 
-export default function ProductCard({ product }: { product: Product }) {
+const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   const { addToCart } = useCart();
 
   const handleAddToCart = () => {
     addToCart(product);
-    console.log('Added to cart:', product); // Add this line for debugging
   };
 
   return (
-    <div className="border rounded-lg p-4">
-      <Image src={product.image_url} alt={product.name} width={500} height={500} className="w-full h-48 object-cover mb-4" />
-      <h2 className="text-xl font-semibold">{product.name}</h2>
-      <p className="text-gray-600">{product.description}</p>
-      <p className="text-lg font-bold mt-2">₲{product.price.toLocaleString()}</p>
-      <button
-        onClick={handleAddToCart}
-        className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 w-full"
-      >
-        Add to Bag
-      </button>
+    <div key={product.id}>
+      <div className="relative">
+        <div className="relative h-72 w-full overflow-hidden rounded-lg">
+          <img
+            alt={product.name}
+            src={product.image_url}
+            className="h-full w-full object-cover object-center"
+          />
+        </div>
+        <div className="relative mt-4">
+          <h3 className="text-sm font-medium text-gray-900">{product.name}</h3>
+          <p className="mt-1 text-sm text-gray-500">{product.description}</p>
+        </div>
+        <div className="absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4">
+          <div
+            aria-hidden="true"
+            className="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50"
+          />
+          <p className="relative text-lg font-semibold text-white">₲{product.price.toLocaleString()}</p>
+        </div>
+      </div>
+      <div className="mt-6">
+        <button
+          onClick={handleAddToCart}
+          className="relative flex w-full items-center justify-center rounded-md border border-transparent bg-gray-100 px-8 py-2 text-sm font-medium text-gray-900 hover:bg-gray-200"
+        >
+          Agregar al carrito<span className="sr-only">, {product.name}</span>
+        </button>
+      </div>
     </div>
   );
-}
+};
+
+export default ProductCard;
