b688e5847baee6b64e2d00ce06d20cdb200cab88
Joelft94 <joel_ferrer_94@hotmail.com>
2025-01-06 14:00:58 -0300

    fix: setIsOpen

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -37,7 +37,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
         </div>
         <div className="relative mt-4">
           <h3 className="text-sm font-medium text-gray-900">{product.name}</h3>
-          <p className="mt-1 text-sm text-gray-500">{isOpen? product.description : product.description.substring(0, 10) } </p>
+          <p className="mt-1 text-sm text-gray-500">{isOpen? product.description : product.description.substring(1, 5)} </p>
         </div>
         <div className="absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4">
           <div
