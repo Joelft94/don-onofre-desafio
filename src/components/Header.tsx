056a15667921663e4ea9436407476e458a3f05e3
Joel Ferreira <joel_ferrer_94@hotmail.com>
2025-01-05 23:11:29 -0300

    update: title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -54,7 +54,7 @@ const Header: React.FC = () => {
         <div className="mt-2 md:flex md:items-center md:justify-between">
           <div className="min-w-0 flex-1">
             <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-              Don Onofre
+              Encuentra tu servicio
             </h2>
           </div>
           <div className="mt-4 flex flex-shrink-0 md:ml-4 md:mt-0">
